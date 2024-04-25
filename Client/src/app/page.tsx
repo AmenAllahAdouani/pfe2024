3f3582d885ef7d85a9b647c41ca311784c871849
Amen-Allah <ameallah07@gmail.com>
2024-04-25 07:26:04 +0200

    ll

--- Client/src/app/page.tsx
+++ Client/src/app/page.tsx
@@ -15,14 +15,16 @@ import SigninForm from "./_auth/forms/SigninForm";
 import Settings from "./Molecules/Settings";
 import Main from "./Main";
 import Reviews from "./Molecules/Reviews";
+import Welcome from "./Molecules/Welcome";
 
 export default function Home() {
   return (
     <main>
-      <Reviews />
-     
+
+      <Welcome />
 
       {/*
+      <Reviews />
       <Settings></Settings>
 
       <ProgressProvider>
