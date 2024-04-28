fe8075948f5eb850a2a4f156d10cbcd439779bdc
Amen-Allah <ameallah07@gmail.com>
2024-04-28 08:27:59 +0200

    15

--- Server/router/Reviews.js
+++ Server/router/Reviews.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const Reviews = require('../controller/Reviews');
 
 // Define a route to handle POST requests to create a new review 
-router.post('/reviews', Users.createReview);
-router.patch('/update/:id', Users.updateReview);
+router.post('/reviews', Reviews.createReview);
+router.patch('/update/:id', Reviews.updateReview);
 module.exports = router;
